c83c2fa17fb8691ead249e70cc7e9ca4669d3bbc
Michael Stringer <mstringer09@yahoo.com>
2019-01-17 22:09:44 -0600

    added file

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(_dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+app.listen(process.env.PORT || 3000);
